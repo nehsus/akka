e3f970c69c82cdf97f3ed54e6b73186a5acdc22a
Sushen Kumar <skumar88@uic.edu>
2021-12-10 10:36:01 -0600

    added src, cleanup

---
+++ ActorSecret/src/main/java/edu/uic/cs554/project/utils/Constant.java
@@ -0,0 +1,10 @@
+package edu.uic.cs554.project.utils;
+
+import java.io.File;
+
+/**
+ * Utility class for constants
+ */
+public class Constant {
+    public static final File actorYAML = new File("src/main/resources/actors.yaml");
+}
